9ae44b643f7a4397f7d0c56526e374a7877863a2
Mallory Row <Mallory@Malcintosh.local>
2025-06-02 10:23:44 -0400

    Add Swagger documentation

--- src/app.py
+++ src/app.py
@@ -2,13 +2,29 @@ import sqlite3
 from flask import Flask, request, jsonify, g
 import statistics
 import os
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
 RAW_DATABASE = '../raw_wx_data.db'
 STATS_DATABASE = '../stats_wx_data.db'
 
-# Connect and read to databases
+# Configuring Swagger
+template = {
+    "swagger": "2.0",
+    "info": {
+      "title": "Corteva Code Challenge Weather API",
+      "description": "This API was developed by Mallory Row (mallory.row@gmail.com) for the Corteva Code Challenge.",
+      "version": "1.0"
+    }
+}
+app.config['SWAGGER'] = {
+    'title': 'Corteva Code Challenge Weather API',
+    'uiversion': 2,
+    'template': './resources/flasgger/swagger_ui.html'
+}
+swagger = Swagger(app, template=template)
 
+#### Connect and read to databases
 def get_db(DATABASE):
     db = getattr(g, '_database', None)
     if db is None:
@@ -29,6 +45,26 @@ def init_stats_db():
 #### Routes
 @app.route('/api/weather/', methods=['GET'])
 def weather():
+    """
+    This displays daily weather data.
+    The stations are in Nebraska, Iowa, Illinois, Indiana, or Ohio.
+    There are many entries but the default limit is to 10.
+    ---
+    tags:
+      - Weather
+    responses:
+      200:
+        description: Daily weather data
+        examples:
+          application/json:
+            status: success
+            data:
+              Date: YYYYMMDD format
+              MaxT: maximum temperature (Celsius)
+              MinT: minimum temperature (Celsius)
+              Precip: Total precipitation (millimeters)
+              StationID: station identification
+    """
     raw_db = get_db(RAW_DATABASE)
 
     if request.method == 'GET':
@@ -56,6 +92,26 @@ def weather():
 
 @app.route('/api/weather/stats/', methods=['GET'])
 def stats():
+    """
+    This displays annual weather data.
+    The stations are in Nebraska, Iowa, Illinois, Indiana, or Ohio.
+    There are many entries but the default limit is to 10.
+    ---
+    tags:
+      - Weather
+    responses:
+      200:
+        description: Annual weather data
+        examples:
+          application/json:
+            status: success
+            data:
+              AvgMaxT: yearly average maximum temperature (Celsius)
+              AvgMinT: yearly average minimum temperature (Celsius)
+              TotalPrecip: Total yearly precipitation (millimeters)
+              Year: YYYY format
+              StationID: station identification
+    """
     stats_db = get_db(STATS_DATABASE)
 
     if request.method == 'GET':
